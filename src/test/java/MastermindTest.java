d58d9e7ae3fa430133e69a8137c2061d20fae5fe
Oge Nnadi <onnadi@pillartechnology.com>
2019-04-02 12:25:15 -0500

    Fix formatting

--- src/test/java/MastermindTest.java
+++ src/test/java/MastermindTest.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class MastermindTest {
-    public boolean isMisplaced(ArrayList<Integer> secret, ArrayList<Integer> guess, int index){
+    public boolean isMisplaced(ArrayList<Integer> secret, ArrayList<Integer> guess, int index) {
         return secret.contains(guess.get(index)) &&
                 secret.get(index) != guess.get(index);
     }
 
-    public boolean isWellPlaced(ArrayList<Integer> secret, ArrayList<Integer> guess, int index){
+    public boolean isWellPlaced(ArrayList<Integer> secret, ArrayList<Integer> guess, int index) {
         return secret.get(index) == guess.get(index);
     }
 
-    public interface TwoListIterator{
-        boolean test(ArrayList<Integer> a, ArrayList<Integer>b, int index);
+    public interface TwoListIterator {
+        boolean test(ArrayList<Integer> a, ArrayList<Integer> b, int index);
     }
 
     public int count(ArrayList<Integer> secret, ArrayList<Integer> guess, TwoListIterator iterator) {
@@ -29,10 +29,10 @@ public class MastermindTest {
         return total;
     }
 
-    public ArrayList<Integer> list(int... i) {
+    private ArrayList<Integer> list(int... i) {
         ArrayList<Integer> returnValue = new ArrayList<>();
 
-        for(int item: i){
+        for (int item : i) {
             returnValue.add(item);
         }
 
@@ -53,17 +53,17 @@ public class MastermindTest {
     }
 
     @Test
-    public void givenOneWellPlacedAndOneWrongReturn1_0(){
+    public void givenOneWellPlacedAndOneWrongReturn1_0() {
         assertEquals(list(1, 0), evaluate(list(1, 2), list(1, 3)));
     }
 
     @Test
-    public void givenTotallyWrongGuessReturn0_0(){
+    public void givenTotallyWrongGuessReturn0_0() {
         assertEquals(list(0, 0), evaluate(list(1), list(2)));
     }
 
     @Test
-    public void givenCorrectGuessReturnSizeOfBoard_0(){
+    public void givenCorrectGuessReturnSizeOfBoard_0() {
         assertEquals(list(2, 0), evaluate(list(2, 3), list(2, 3)));
     }
 }
